5d4c193d12ab77916882434fed205291df0cb1db
Fedricco <fedriccorabaya@gmail.com>
2018-03-26 12:18:57 -0700

    Updated Database

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
 <div class="uk-section uk-section-large uk-section-primary black">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+            <div class="panel panel-default black">
                 <div class="panel-heading">Login</div>
 
                 <div class="panel-body">
